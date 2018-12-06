4d9eb1a6c337374de741d17d172ade6e0eb9fcc1
Richard Decal <richard.decal@ncf.edu>
2018-12-06 06:48:28 -0500

    oped predictions

--- doc2vec_model_benchmarks.py
+++ doc2vec_model_benchmarks.py
@@ -26,7 +26,8 @@ from sklearn import metrics
 import string
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 import multiprocessing
-
+import collections
+import seaborn as sns
 
 target_names = ["Chicken", "Punk", "perp", "Garbage", "Scum", "Toilet",
                 "Poop", "Yuck"]
@@ -34,7 +35,6 @@ target_names = ["Chicken", "Punk", "perp", "Garbage", "Scum", "Toilet",
 # Display progress logs on stdout
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(message)s')
-
 
 # parse commandline arguments
 op = OptionParser()
@@ -66,8 +66,6 @@ op.add_option("--filtered",
                    "headers, signatures, and quoting.")
 
 
-
-
 def is_interactive():
     return not hasattr(sys.modules['__main__'], '__file__')
 
@@ -86,10 +84,9 @@ print(__doc__)
 op.print_help()
 print()
 
-
 # #############################################################################
 categories = ["Chicken", "Punk", "perp", "Garbage", "Scum", "Toilet",
-                "Poop", "Yuck"]
+              "Poop", "Yuck"]
 
 with open('Dan Coats.txt', 'r+') as in_file:
     textDan = in_file.read()
@@ -152,10 +149,10 @@ train = pd.concat(
     [pdDan, pdMattis, pdKelly, pdHassett, pdNielsen, pdKudlow, pdPence,
      pdPompeo])
 
-author_to_num ={'Dan Coats': "Chicken", 'James Mattis': "Punk",
-                'John Kelly': "perp", 'Kevin Hassett': "Garbage",
-                  'Kirstjen Nielsen': "Scum", 'Larry Kudlow': "Toilet",
-                'Mike Pence': "Poop", 'Mike Pompeo': "Yuck"}
+author_to_num = {'Dan Coats': "Chicken", 'James Mattis': "Punk",
+                 'John Kelly': "perp", 'Kevin Hassett': "Garbage",
+                 'Kirstjen Nielsen': "Scum", 'Larry Kudlow': "Toilet",
+                 'Mike Pence': "Poop", 'Mike Pompeo': "Yuck"}
 
 train["author"].replace(author_to_num, inplace=True)
 author = train['author'].tolist()
@@ -167,34 +164,33 @@ double spaces,
 NEW LINES
 other unicode
 """
-text = [t.replace('\xa0', ' ').replace("\u2028", " ").replace(u'\ufeff',' ') \
-        .replace('\n', " ").replace("  ", " ") for t in text]
+text = [t.replace('\xa0', ' ').replace("\u2028", " ").replace(u'\ufeff', ' ') \
+            .replace('\n', " ").replace("  ", " ") for t in text]
 
 print("there are ", len(text), "sentences")
 
-def random_generator(size=6, chars=list(string.ascii_uppercase + string.digits)):
+
+def random_generator(size=6,
+                     chars=list(string.ascii_uppercase + string.digits)):
     return ''.join(list(np.random.choice(chars, size)))
 
 
-X_txt_train, X_txt_test, y_train, y_test = train_test_split(text, author, test_size=0.25, random_state=1337)
+X_txt_train, X_txt_test, y_train, y_test = train_test_split(text, author,
+                                                            test_size=0.25,
+                                                            random_state=1337)
 X_txt_train = [TaggedDocument(doc, [random_generator()]) for doc in X_txt_train]
 X_txt_test = [TaggedDocument(doc, [random_generator()]) for doc in X_txt_test]
-
-
 
 N_DIMS = 100
 N_EPOCHS = 50
 
-
 vectorizer = Doc2Vec(seed=1,
-            workers=multiprocessing.cpu_count(),
-            vector_size=N_DIMS,
-            dm=0,  # use distributed bag of words
-            min_count=0,
-            window=15,
-            epochs=N_EPOCHS)
-
-
+                     workers=multiprocessing.cpu_count(),
+                     vector_size=N_DIMS,
+                     dm=0,  # use distributed bag of words
+                     min_count=0,
+                     window=15,
+                     epochs=N_EPOCHS)
 
 vectorizer.build_vocab(X_txt_train)
 vectorizer.train(X_txt_train, total_examples=vectorizer.corpus_count,
@@ -208,6 +204,8 @@ print("Extracting features from the test data using the same vectorizer")
 t0 = time()
 X_test = [vectorizer.infer_vector(document.words) for document in X_txt_test]
 duration = time() - t0
+
+
 # print("n_samples: %d, n_features: %d" % X_test.shape)
 # print()
 
@@ -255,9 +253,10 @@ def benchmark(clf):
     accuracy = metrics.accuracy_score(y_test, pred)
     print("accuracy:   %0.3f" % accuracy)
 
-    precision, recall, fscore, _support = metrics.precision_recall_fscore_support(y_test, pred,
-                                               average="macro")
-    #g
+    precision, recall, fscore, _support = metrics.precision_recall_fscore_support(
+        y_test, pred,
+        average="macro")
+    # g
 
     if opts.print_report:
         print("classification report:")
@@ -285,10 +284,9 @@ for penalty in ["l2", "l1"]:
     results.append(benchmark(LinearSVC(penalty=penalty, dual=False,
                                        tol=1e-3)))
 
-
 # Train sparse Naive Bayes classifiers
 print('=' * 80)
-print("Naive Bayes")
+# print("Naive Bayes")
 # results.append(benchmark(MultinomialNB(alpha=.01)))
 # results.append(benchmark(BernoulliNB(alpha=.01)))
 
@@ -311,12 +309,56 @@ plt.barh(indices + .6, fscore, .2, label="F-Score")
 
 plt.yticks(())
 plt.legend(loc='best')
-#plt.subplots_adjust(left=.25)
+plt.subplots_adjust(left=.25)
 plt.subplots_adjust(top=.95)
 plt.subplots_adjust(bottom=.05)
 
 for i, c in zip(indices, clf_names):
-    plt.text(-0.15, i, c)
+    plt.text(-0.12, i, c)
 
-plt.savefig("doc2vec benchmarks.png")
+# plt.savefig("doc2vec benchmarks.png")
+plt.show()
+
+## oped prediction
+opEd = pd.read_table("OpEd.txt", header=None)
+opEd.columns = ['text']
+
+NYT_data = opEd['text'].tolist()
+NYT_data = [TaggedDocument(doc, [random_generator()]) for doc in X_txt_test]
+X_NYT = [vectorizer.infer_vector(document.words) for document in X_txt_train]
+
+all_writing_samples = [TaggedDocument(doc, [random_generator()]) for doc in
+                       text]
+all_writing_samples_embedded = [vectorizer.infer_vector(document.words) for
+                                document in all_writing_samples]
+
+best_classifier = LinearSVC(penalty="l2", dual=False, tol=1e-3)
+best_classifier.fit(all_writing_samples_embedded, author)
+
+predictions = best_classifier.predict(X_NYT)
+
+predictedAuthordf = pd.DataFrame(predictions)
+predictedAuthordf.columns = ['Author']
+predictedAuthordf = predictedAuthordf['Author'].value_counts().reset_index()
+predictedAuthordf = pd.DataFrame(predictedAuthordf)
+predictedAuthordf.columns = ['Author', 'Count']
+predictedAuthordf["Probability"] = predictedAuthordf["Count"] / (
+    predictedAuthordf['Count'].sum())
+predictedAuthordf["logLikelihood"] = np.log(predictedAuthordf["Probability"])
+
+prediction = predictedAuthordf['logLikelihood'].idxmax()
+predictedAuthor = predictedAuthordf.at[prediction, 'Author']
+
+print(predictedAuthordf, "\n\n")
+
+# predictedAuthor = (list(possibleAuthors.keys())[list(possibleAuthors.values()).index(predictedAuthor)])
+print("The predicted author is: ", predictedAuthor)
+
+# print(predictions)
+bins = len(set(predictions))
+plt.hist(predictions, density=True, align="mid", bins=bins)
+plt.xticks(range(bins))
+plt.ylabel("% OpEd Sentences Attributed to Author", fontsize=16)
+plt.xlabel("Author", fontsize=16)
+plt.title("Percent of Sentences Attributed to the Authors", fontsize=20)
 plt.show()
